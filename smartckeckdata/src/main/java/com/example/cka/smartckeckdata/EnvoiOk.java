ed707fecfb2ffedbafd0cd9d3252d9b796170d36
SDTECH01 <smartdatatechnologie@gmail.com>
2018-06-26 12:04:31 +0000

    2 test matinal

--- smartckeckdata/src/main/java/com/example/cka/smartckeckdata/EnvoiOk.java
+++ smartckeckdata/src/main/java/com/example/cka/smartckeckdata/EnvoiOk.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.Request;
 
@@ -58,6 +60,25 @@ public class EnvoiOk{
         });
     }
 
+    public static void EnvoiFinal()
+            throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        RequestBody formBody = new FormBody.Builder()
+                .add("nom", "nom")
+                .add("numero", "2548754")
+                .build();
+
+        Request request = new Request.Builder()
+                .url("http://smart-data-tech.com/dev/fr/crud.php")
+                .post(formBody)
+                .build();
+
+        Call call = client.newCall(request);
+        Response response = call.execute();
+
+
+        Log.i("est votre code", String.valueOf(response.code()));
+    }
     /*@Override
     public void onFailure(Call call, IOException e) {*/
         /*void runOnUiThread(new Runnable() {
